136b74b74e58c2f44fd8289354fead2c51b05028
Hebert Santana <102166830+hebert-santana@users.noreply.github.com>
2023-03-11 17:45:47 -0300

    delete DSA

--- Atividades_DSA/Cap06/10.py
+++
@@ -1,7 +0,0 @@
-# Exercício 10 - Crie um regex em Python para extrair a palavra que aparece depois das palavras Data e Science na frase: 'A Data Science Academy oferce os melhores cursos de análise de dados do Brasil.'
-import re
-texto = 'A Data Science Academy oferece os melhores cursos de análise de dados do Brasil.'
-
-resultado = re.findall(r'Data Science (\w+)', texto)
-print("Resultado:", resultado[0])
-
